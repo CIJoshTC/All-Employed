cfe08e71132c03d1aa71803bc0617c6354c6f59a
Joshua <23joshtc@gmail.com>
2023-08-10 19:47:52 -0400

    added more fucntions to queries.js employees, and roles

--- queries.js
+++ queries.js
@@ -11,9 +11,30 @@ async function getAllDepartments() {
         throw error;
     }
 }
+// Function to retrieve all employees using Promises
+async function getAllEmployees() {
+    try {
+        const [rows, fields] = await pool.query('SELECT * FROM employee');
+        return rows;
+    } catch (error) {
+        console.error('Error fetching employees:', error);
+        throw error;
+    }
+}
+async function getAllRoles() {
+    try {
+        const [rows, fields] = await pool.query('SELECT * FROM role');
+        return rows;
+    } catch (error) {
+        console.error('Error fetching roles:', error);
+        throw error;
+    }
+}
 
 // Add more query functions as needed
 module.exports = {
-    getAllDepartments
+    getAllDepartments,
+    getAllEmployees,
+    getAllRoles
     // Add more exported functions here
 };
